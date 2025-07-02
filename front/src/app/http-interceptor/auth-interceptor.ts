0f00829ea172931fde2c31d4fd99b7254585ca05
ndudnicz <ndudnicz@protonmail.com>
2025-07-02 12:25:30 +0200

    front: exclude refresh token route from http interceptor

--- front/src/app/http-interceptor/auth-interceptor.ts
+++ front/src/app/http-interceptor/auth-interceptor.ts
@@ -3,6 +3,9 @@ import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
 export const authInterceptorFn: HttpInterceptorFn = (req, next) => {
+    if (req.url.endsWith('/auth/refresh-token')) {
+        return next(req);
+    }
     const authService = inject(AuthService);
     const token = authService.getJwtToken();
     const cloned = req.clone({
